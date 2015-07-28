0b9c1fb7adf170348e3d9beb03e89ec0592f2a59
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-28 14:34:00 -0500

    created albums table

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	artist VARCHAR(100) NOT NULL,
+	record_name VARCHAR(100) NOT NULL,
+	release_date YEAR NOT NULL,
+	copies_sold FLOAT(15, 2) NOT NULL,
+	genres VARCHAR(50) NOT NULL,
+	PRIMARY KEY(id)
+);
