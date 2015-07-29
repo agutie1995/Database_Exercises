8f78c27c70866c05f631f7e1ac0341111b75162a
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-29 10:46:15 -0500

    wrote SELECT statements

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE albums (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	artist VARCHAR(100) NOT NULL,
 	record_name VARCHAR(100) NOT NULL,
-	release_date YEAR NOT NULL,
+	release_date INT NOT NULL,
 	copies_sold FLOAT(15, 2) NOT NULL,
 	genres VARCHAR(50) NOT NULL,
 	PRIMARY KEY(id)
