92e8f1af54ceeb1ee02b2b269919e9c514689172
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-30 09:27:03 -0500

    Used DISTINCT to find unique tites, updated to show in alpha order

---
+++ group_by_exercise.sql
@@ -0,0 +1,5 @@
+ -- Use DISTINCT to find the unique titles in the titles table
+ 	-- Update the previous query to sort the results alphabetically.
+SELECT DISTINCT title
+FROM titles
+ORDER BY title ASC
