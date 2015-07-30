9ceadc2161e3e6328eb8bc9791f2b85a93ad049b
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-30 09:35:04 -0500

    Used DELETE and UPDATE to edit album table

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE albums (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	artist VARCHAR(100) NOT NULL,
 	record_name VARCHAR(100) NOT NULL,
-	release_date INT NOT NULL,
+	release_date INT(255) NOT NULL,
 	copies_sold FLOAT(15, 2) NOT NULL,
 	genres VARCHAR(50) NOT NULL,
 	PRIMARY KEY(id)
--- delete_exercises.sql
+++ delete_exercises.sql
@@ -2,9 +2,12 @@ USE codeup_test_db;
 
 -- Albums released after 1991
 SELECT * FROM albums WHERE release_date > 1991;
+-- DELETE FROM albums WHERE release_date > 1991;
 
 -- Albums with the genre "disco"
 SELECT * FROM albums WHERE genres = 'Disco';
+-- DELETE FROM albums WHERE genres = 'Disco';
 
 -- Albums by "Whitney Houston" (...or maybe an artist of your choice)
 SELECT * FROM albums WHERE artist LIKE '%Whitney Houston%';
+-- DELETE FROM albums WHERE artist LIKE '%Whitney Houston%';
