ba995bd79937b3d3a089bcb59aafc0a2cb721d82
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-30 09:32:03 -0500

    updated to find unique combinations for first and last names

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -7,8 +7,8 @@ ORDER BY title ASC
 -- Find query for employees whose last names start and end with 'E'
 	-- Update query find just the unique last names that start and end with 'E' using GROUP BY
 	-- Update previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'
-SELECT last_name, first_name
+SELECT concat(first_name, ' ', last_name) AS full_name
 FROM employees
 WHERE (last_name LIKE 'E%'
 	AND last_name LIKE '%e')
-GROUP BY first_name, last_name;
+GROUP BY full_name;
