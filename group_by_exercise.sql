2050451474fad12e84a54d8942715903215bcaf5
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-30 09:31:33 -0500

    used group by to find unique last names

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -3,3 +3,12 @@
 SELECT DISTINCT title
 FROM titles
 ORDER BY title ASC
+
+-- Find query for employees whose last names start and end with 'E'
+	-- Update query find just the unique last names that start and end with 'E' using GROUP BY
+	-- Update previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'
+SELECT last_name, first_name
+FROM employees
+WHERE (last_name LIKE 'E%'
+	AND last_name LIKE '%e')
+GROUP BY first_name, last_name;
